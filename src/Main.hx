2b2bfad8aec34d51c8f8db49080f1e4ec2ead39b
Josh Chandler <j5d1c7@gmail.com>
2021-03-19 21:51:07 -0400

    image output

--- src/Main.hx
+++ src/Main.hx
@@ -1,7 +1,11 @@
+import Image.gridToPNG;
+
 class Main {
 	static function main() {
-		var g = new Grid(6, 20);
-		SidewinderMaze.on(g);
+		var g = new Grid(64, 64);
+		// SidewinderMaze.on(g);
+		BinaryTreeMaze.on(g);
 		g.print();
+		gridToPNG(g);
 	}
 }
--- src/Useful.hx
+++ src/Useful.hx
@@ -2,3 +2,7 @@ function sampleArray(array:Array<Dynamic>) {
     var idx = Std.random(array.length);
     return array[idx];
 }
+
+inline function intAbs(i:Int) {
+    return if (i < 0) -i else i;
+}
